81580666152fb6416accb6a873bba4317cfb17ec
sunshinejen <35351160+sunshinejen@users.noreply.github.com>
2018-05-02 21:22:10 -0700

    Add files via upload

---
+++ big/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title><?=$title?></title>
+<meta name="viewport" content="width=device-width" />
+<meta name="robots" content="noindex,nofollow" />
+<meta charset="utf-8">
+<script src="https://use.fontawesome.com/6a71565c22.js"></script>
+<link rel="stylesheet" href="../big/css/nav.css" />
+<link rel="stylesheet" href="../big/css/big.css" />
+<link rel="stylesheet" href="../big/css/form.css" />
+<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script src="http://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
+<script src="js/script.js"></script>    
+</head>
+
+<body>
+<!-- START WRAPPER -->
+<div class="wrapper">
+<header>
+  <h1><a href="index.php"><i class="logo fa fa-home"></i>WEB120 Portal by Jennifer Villacis</a></h1>
+  <nav id="cssmenu">
+          <ul>
+             <li><a href="../index.php"><span><i class="fa fa-fw fa-bank"></i> WEB120</span></a></li>
+             <li><a href="index.php"><span><i class="fa fa-fw fa-home"></i> BIG</span></a></li>
+              <li><a href="#"><span><i class="fa fa-fw fa-chevron-down"></i>Web Development Research</span></a>
+                  <ul>
+                      <li><a href="flexbox.php"><span><i class="fa fa-fw fa-cube"></i> Flexbox</span></a></li>
+                      <li><a href="galleries.php"><span><i class="fa fa-fw fa-camera-retro"></i> Galleries</span></a></li>
+                      <li><a href="parallax.php"><span><i class="fa fa-fw fa-fighter-jet"></i> Parallax</span></a></li>
+                      <li><a href="shoppingcarts.php"><span><i class="fa fa-fw fa-shopping-basket"></i> Shopping Carts</span></a></li>
+             <li><a href="siteapp.php"><span><i class="fa fa-fw fa-html5"></i> Site vs App</span></a></li>
+                  </ul>
+             
+             <li><a href="#"><span><i class="fa fa-fw fa-chevron-down"></i> Google</span></a>
+                <ul>
+                   <li><a href="calendar.php"><span><i class="fa fa-fw fa-calendar"></i> Calendar</span></a></li>
+                   <li><a href="map.php"><span><i class="fa fa-fw fa-map-o"></i> Map</span></a></li>
+                   <li><a href="youtube.php"><span><i class="fa fa-fw fa-youtube-square"></i> YouTube</span></a></li>
+                </ul>
+             </li>
+             <li><a href="webcam.php"><span><i class="fa fa-fw fa-eye"></i> Web Cam</span></a></li>
+     </ul>
+  </nav>
+</header>
+    
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Welcome</h2>
+ <!-- END header.php include here! -->
+<!-- Add multiple.php contact form address questions we ask most web clients -->
+
+</section>
+<!-- END LEFT COL -->
+
+    <!-- START RIGHT COL -->
+<aside>
+<h3>Additonal Resources</h3>
+<p>Provide at least 3 (linked) resources that help you and/or your web team with steps found in very basic website design and development cycle. For example, client surveys, wireframing tools, etc.</p>
+<p>Here's a website to help get you started, https://business.tutsplus.com/articles/...</p>
+</aside>
+<!-- END RIGHT COL -->
+
+<!-- START Footer -->
+<footer>
+  <p><small>&copy; 2018 by <a href="contactme.php">Jennifer Villacis</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+</footer>
+<!-- END Footer --> 
+</div>
+<!-- END WRAPPER -->
+</body>
+</html>
